31bbaeab24426ce1145dc18b71c5e50ec68440ee
Josh Emery <josh.emery@smartfleet.co.uk>
2022-02-25 12:09:31 +0000

    Refactor Game Class Methods Pt 1

--- src/Game.ts
+++ src/Game.ts
@@ -2,27 +2,56 @@ export class Game {
     private _lastSymbol: string = ' ';
     private _board: Board = new Board();
 
-    public Play(symbol: string, x: number, y: number) : void {
-        //if first move
+    /*  Code Smells -->
+        Comments - if the methods/parameters were more though out comments wouldn't be needed
+        Large Class - could this class be shortend?
+        Long Method - > 10 lines - looks like it could be easily refactored into
+                      smaller methods, which would also help remove comments by 
+                      including good method descriptions
+
+
+     */
+
+    private ensureXPlaysFirst(symbol:string) {
         if (this._lastSymbol == ' ') {
-            //if player is X
             if (symbol == 'O') {
                 throw new Error("Invalid first player");
             }
         }
-        //if not first move but player repeated
-        else if (symbol == this._lastSymbol) {
-            throw new Error("Invalid next player");
-        }
-        //if not first move but play on an already played tile
-        else if (this._board.TileAt(x, y).Symbol != ' ') {
-            throw new Error("Invalid position");
-        }
+    }  
 
-        // update game state
-        this._lastSymbol = symbol;
-        this._board.AddTileAt(symbol, x, y);
+    private ensurePlayerNotRepeated(symbol:string) {
+        if (symbol == this._lastSymbol) {
+            throw new Error("Invalid next player");
+        }    
     }
+
+    private ensureTileIsEmpty(symbol:string,x: number, y: number){
+        if (this._board.TileAt(x, y).Symbol != ' ') {
+            throw new Error("Invalid position");
+        }        
+    }
+
+    private updateGameState(symbol:string,x: number, y: number){
+        this._lastSymbol = symbol;
+        this._board.AddTileAt(symbol, x, y);            
+    }
+
+    public Play(symbol: string, x: number, y: number) : void {
+        this.ensureXPlaysFirst(symbol);
+        this.ensurePlayerNotRepeated(symbol);
+        this.ensureTileIsEmpty(symbol, x, y);
+        this.updateGameState(symbol, x, y);
+    }
+
+        // Incomplete refactor ->
+//     private checkRowIsSameSymbol(symbol:string, row:number): Boolean {
+//         if (this._board.TileAt(0, 0)!.Symbol ==
+//         this._board.TileAt(0, 1)!.Symbol &&
+//         this._board.TileAt(0, 2)!.Symbol == this._board.TileAt(0, 1)!.Symbol) {
+//     return this._board.TileAt(0, 0)!.Symbol;
+// }        
+
 
     public Winner() : string {
         //if the positions in first row are taken
